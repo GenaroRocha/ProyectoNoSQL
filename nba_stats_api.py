ccb8611a3c4e9cc3cc5c3ead6a424cd1897a2f48
Luis Genaro Sánchez Rocha <67811130+GenaroRocha@users.noreply.github.com>
2023-11-29 10:36:56 -0600

    Update nba_stats_api.py

--- nba_stats_api.py
+++ nba_stats_api.py
@@ -1,19 +1,31 @@
 import requests
 from pymongo import MongoClient
 
-# Realizar la solicitud a la API
+client = MongoClient('mongodb://localhost:27017/')
+db = client['nba']
+collection = db['player_data']
+
 for season in range(2010, 2024):
-	response = requests.get('https://nba-stats-db.herokuapp.com/api/playerdata/season/' + str(season))
-	data = response.json()
+    try:
+        # Realizar la solicitud a la API
+        response = requests.get(f'https://nba-stats-db.herokuapp.com/api/playerdata/season/{season}')
+        response.raise_for_status()  # Esto lanzará una excepción si la solicitud no fue exitosa
+        data = response.json()
 
-	# Conectar a MongoDB
-	client = MongoClient('mongodb://localhost:27017/')
-	db = client['nba']
-	collection = db['player_data']
+        # Insertar datos en MongoDB si 'results' está presente en la respuesta
+        if 'results' in data:
+            collection.insert_many(data['results'])
+        else:
+            print(f"No se encontraron resultados para la temporada {season}")
+    
+    except requests.RequestException as e:
+        print(f"Error de solicitud para la temporada {season}: {e}")
+    except pymongo.errors.PyMongoError as e:
+        print(f"Error al insertar documentos para la temporada {season}: {e}")
 
-	# Insertar datos en MongoDB
-	collection.insert_many(data['results'])
-
+# Contar y mostrar el número de documentos en la colección
+count = collection.count_documents({})
+print(f"Total de documentos en la colección: {count}")
 
 
 
